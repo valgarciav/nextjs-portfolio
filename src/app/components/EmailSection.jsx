8ea309715348670a841f4f1024ba8fcd48445871
Valeria Garcia <97136074+valgarciav@users.noreply.github.com>
2023-11-21 23:34:53 -0500

    email form is now working

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,12 +1,32 @@
-
-import React from "react";
+"use client"
+import React, { useRef } from 'react';
 import GithubIcon from "/public/image/github-icon.svg";
 import LinkedinIcon from "/public/image/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
+import emailjs from '@emailjs/browser';
 
 
 const EmailSection = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_3jq9jha", "template_xx9dzd5", form.current,'qr-bha9pEfH-lwXuV')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+          alert("SUCCESS! YOUR EMAIL IS NOW IN VALERIA INBOX");
+      }, (error) => {
+          console.log(error.text);
+          alert("ERROR!");
+          
+      }
+      );
+      e.target.reset();
+  };
+
   return (
     <section
     id="contact" 
@@ -16,7 +36,7 @@ const EmailSection = () => {
             </div>        
             <div className="z-10">
             <h5 className='text-xl font-bold text-white my-2'>
-            Let`&apos;s Connect
+            Let&apos;s Connect
             </h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {" "}
@@ -40,7 +60,66 @@ const EmailSection = () => {
             </div>
         </div>
         <div>
-            <form className='flex flex-col'>
+            <form ref={form} onSubmit={sendEmail}>
+                <div className="mb-6">
+                <label
+                htmlFor="email"  
+                className='text-white block mb-2 text-sm font-medium'
+                >
+                Your Email
+                </label>
+                <input 
+                type="text" 
+                id="email"
+                name="email" 
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" 
+                placeholder='helloworld@gmail.com' 
+                />
+                </div>
+
+                <div className="mb-6">
+                <label
+                htmlFor="subject" 
+                className='text-white block text-sm mb-2 font-medium'
+                >
+                Subject
+                </label>
+                <input 
+                type="text"
+                name="subject" 
+                id="subject" 
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" 
+                placeholder='Just saying hi' 
+                />
+                </div>
+
+                <div className="mb-6">
+                    <label
+                    htmlFor="message"
+                    className="text-white block text-sm mb-2 font-medium"
+                    >
+                    Message
+                    </label>
+                    <textarea
+                    name="message"
+                    id="message"
+                    className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" 
+                    placeholder="Let's talk about..."
+                    />
+                    </div>
+                    <button 
+                    type="submit"
+                    className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
+                        Send Message
+                    </button>
+
+            </form>
+
+            
+
+            {/* <form className='flex flex-col'>
                 <div className="mb-6">
                 <label 
                 htmlFor="email" 
@@ -56,6 +135,8 @@ const EmailSection = () => {
                 placeholder='helloworld@gmail.com' 
                 />
                 </div>
+
+
                 <div className="mb-6">
                 <label 
                 htmlFor="subject" 
@@ -71,6 +152,8 @@ const EmailSection = () => {
                 placeholder='Just saying hi' 
                 />
                 </div>
+
+
                 <div className="mb-6">
                     <label
                     htmlFor="message"
@@ -90,7 +173,7 @@ const EmailSection = () => {
                     className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
                         Send Message
                     </button>
-            </form>
+            </form> */}
         </div>
     </section>
   )
