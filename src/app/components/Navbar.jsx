755dfc98de070c6b7357f147f16ca3484499d31f
Valeria Garcia <97136074+valgarciav@users.noreply.github.com>
2023-11-22 18:24:30 -0500

    for real now

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -26,8 +26,8 @@ const Navbar = () => {
         <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
         <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
             <Link href={"/"} 
-            className='text-2xl md:text-5xl text-white font-semibold'>
-                LOGO
+            className='text-2xl md:text-5xl bg-gradient-to-r from-pink-300 via-purple-500 to-blue-400 inline-block text-transparent bg-clip-text font-semibold'>
+                Valeria Garcia
             </Link>
             <div className='mobile-menu block md:hidden'>
                 {
